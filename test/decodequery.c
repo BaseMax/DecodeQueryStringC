53a869511010fe04fdef5056a3ed26778582d7d9
Max Base <MaxBaseCode@Gmail.Com>
2019-03-09 02:00:28 +0330

    Update decodequery.c

--- test/decodequery.c
+++ test/decodequery.c
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : DecodeQueryTest.c
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-03-09
+* @Released under : https://github.com/BaseMax/DecodeQueryStringC/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/DecodeQueryStringC
+*
+**/
