89ccb36aee7d11fc23aa6343012e61509197e6c2
Max Base <MaxBaseCode@Gmail.Com>
2019-03-08 11:41:22 +0330

    Update example.c

--- example.c
+++ example.c
@@ -11,7 +11,6 @@
 #include "decodequery.h"
 int main() {
     char *value=malloc(sizeof(char)*107*2);
-    // A Sample text with utf8 character.
     strcpy(value,"HELLO%2C+%26%231587%3B%26%231604%3B%26%231575%3B%26%231605%3B+%2C+%26%231777%3B%26%231778%3B%26%231779%3B");
     printf("Input: %s\n",value);
     decodeUrl(value,value);
