fd2f97b4b6dd56ce9f6de00d895e6da2ffdb8836
Max Base <MaxBaseCode@Gmail.Com>
2019-03-09 11:44:22 +0330

    Update main.c

--- example/main.c
+++ example/main.c
@@ -3,7 +3,7 @@
 * @Name : Example.c
 * @Version : 1.0
 * @Programmer : Max
-* @Date : 2019-03-08
+* @Date : 2019-03-08, 2019-03-09
 * @Released under : https://github.com/BaseMax/DecodeQueryStringC/blob/master/LICENSE
 * @Repository : https://github.com/BaseMax/DecodeQueryStringC
 *
@@ -14,10 +14,19 @@
 
 int main() {
     char *value=malloc(sizeof(char)*107*2);
-    strcpy(value,"HELLO%2C+%26%231587%3B%26%231604%3B%26%231575%3B%26%231605%3B+%2C+%26%231777%3B%26%231778%3B%26%231779%3B");
+    strcpy(value,"%D8%B3%D9%84%D8%A7%D9%85");
     printf("Input: %s\n",value);
     decodeUrl(value,value);
     // decodeHtmlEntities(value,value);
     printf("Output: %s\n",value);
+
+    printf("\n");
+
+    strcpy(value,"HELLO%2C+%26%231587%3B%26%231604%3B%26%231575%3B%26%231605%3B+%2C+%26%231777%3B%26%231778%3B%26%231779%3B");
+    printf("Input: %s\n",value);
+    decodeUrl(value,value);
+    decodeHtmlEntities(value,value);
+    printf("Output: %s\n",value);
+
     return 0;
 }
